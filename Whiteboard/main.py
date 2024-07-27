e745b83483f13bd2d8e4acabcf78f5001c3e3bc6
Molly Brown <mollybrown616@yahoo.com>
2024-07-27 13:28:45 -0400

    add icon

--- Whiteboard/main.py
+++ Whiteboard/main.py
@@ -4,12 +4,15 @@ from tkinter import ttk
 import tkinter as tk
 
 root = Tk()
+
+# Window title 
 root.title("White Board")
 root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False,False)
 
-
+# Window icon
+root.iconphoto(False, tk.PhotoImage(file="wb.png"))
 
 
 root.mainloop()
