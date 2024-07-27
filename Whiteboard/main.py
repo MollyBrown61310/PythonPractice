a8cbb1e8768defa60679d913722bad4a34837349
Molly Brown <mollybrown616@yahoo.com>
2024-07-27 16:04:09 -0400

    ...

--- Whiteboard/main.py
+++ Whiteboard/main.py
@@ -15,5 +15,21 @@ root.resizable(False,False)
 image_icon = PhotoImage(file="wb.png")
 root.iconphoto(False,image_icon)
 
+#sidebar
+color_box = PhotoImage(file="color section.png")
+Label(root,image=color_box,bg="#f2f3f5").place(x=10,y=20)
+
+eraser=PhotoImage(file="eraser.png")
+Button(root,image=eraser,bg="#f2f3f5").place(x=30,y=400)
+
+importimage=PhotoImage(file="addimage.png")
+Button(root,image=importimage,bg="white").place(x=30,y=450)
+
+colors=Canvas(root,bg="#fff",width=37,height=300,bd=0)
+colors.place(x=30,y=60)
+
+#main screen 
+canvas = Canvas(root,width=930,height=500,background="white",cursor="hand2")
+canvas.place(x=100,y=10)
 
 root.mainloop()
