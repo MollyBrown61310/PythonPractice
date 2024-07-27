364e2a5dcefea841104665f794616a2fbcb85e9b
Molly Brown <mollybrown616@yahoo.com>
2024-07-27 18:42:54 -0400

    Update main.py

--- Whiteboard/main.py
+++ Whiteboard/main.py
@@ -29,7 +29,9 @@ canvas.bind("<Button-1>")
 canvas.bind("<B1-Motion>")
 
 def color_palete():
-    pass
+    id = colors.create_rectangle((10,10,10,30),fill+"Black")
+
+    color_palete()
 
 
 root.mainloop()
