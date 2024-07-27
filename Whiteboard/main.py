098dc514bd8283cb6e7674c83ccbf55b0719b95d
Molly Brown <mollybrown616@yahoo.com>
2024-07-27 16:58:14 -0400

    Update main.py

--- Whiteboard/main.py
+++ Whiteboard/main.py
@@ -9,6 +9,18 @@ root.title("White Board")
 root.geometry("1050x570+130+50")
 root.resizable(False,False)
 
+
+logo = PhotoImage(file="wb.png")
+root.iconphoto(False,logo)
+
+color_panel = PhotoImage(file="color section.png")
+Label(root,image=color_panel,bg='white').place(x=10,y=20)
+
+eraser = PhotoImage(file="eraser.png")
+Button(root,image=eraser,bg="white").place(x=30,y=400)
+
+
+
 root.mainloop()
     
 
