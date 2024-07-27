670cf6de6d7667f7345edd11e68d00814b9f9435
Molly Brown <mollybrown616@yahoo.com>
2024-07-27 13:14:23 -0400

    Update main.py

--- Whiteboard/main.py
+++ Whiteboard/main.py
@@ -9,9 +9,6 @@ root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False,False)
 
-#icon
-image_icon= PhotoImage(file="whiteboardicon.png")
-root.iconphoto(False,image_icon)
 
 
 
