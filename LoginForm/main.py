762991e23596b303898d744c52f89f41484b194d
Molly Brown <mollybrown616@yahoo.com>
2024-07-28 17:36:11 -0400

    Update main.py

--- LoginForm/main.py
+++ LoginForm/main.py
@@ -3,14 +3,14 @@ import tkinter
 window = tkinter.Tk()
 window.title("Login Form")
 window.geometry('340x440')
-#window.configure(bg='#333333')
+window.configure(bg='#333333')
 
-login_label = tkinter.Label(window, text="Login")
-username_label = tkinter.Label(window, text="Username")
+login_label = tkinter.Label(window, text="Login", bg='#333333' , fg="#FFFFFF" )
+username_label = tkinter.Label(window, text="Username", bg='#333333', fg="#FFFFFF")
 username_entry = tkinter.Entry(window)
 password_entry = tkinter.Entry(window, show="*")
-password_label = tkinter.Label(window, text="Password")
-login_button = tkinter.Button(window, text="Login")
+password_label = tkinter.Label(window, text="Password", bg='#333333', fg="#FFFFFF" )
+login_button = tkinter.Button(window, text="Login", bg='#FF3399', fg="#FFFFFF" )
 
 
 login_label.grid(row=0, column=0, columnspan=2)
