25c39457ed486580998cfa47710b77cf54a172fb
Molly Brown <mollybrown616@yahoo.com>
2024-07-28 18:07:09 -0400

    Update main.py

--- LoginForm/main.py
+++ LoginForm/main.py
@@ -5,12 +5,14 @@ window.title("Login Form")
 window.geometry('340x440')
 window.configure(bg='#333333')
 
-login_label = tkinter.Label(window, text="Login", bg='#333333' , fg="#FFFFFF", font=("Arial", 30))
-username_label = tkinter.Label(window, text="Username", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
-username_entry = tkinter.Entry(window, font=("Arial", 16))
-password_entry = tkinter.Entry(window, show="*", font=("Arial", 16))
-password_label = tkinter.Label(window, text="Password", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
-login_button = tkinter.Button(window, text="Login", bg='#FF3399', fg="#FFFFFF", font=("Arial", 16))
+frame = tkinter.Frame()
+
+login_label = tkinter.Label(frame, text="Login", bg='#333333' , fg="#FF3399", font=("Arial", 30))
+username_label = tkinter.Label(frame, text="Username", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+username_entry = tkinter.Entry(frame, font=("Arial", 16))
+password_entry = tkinter.Entry(frame, show="*", font=("Arial", 16))
+password_label = tkinter.Label(frame, text="Password", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+login_button = tkinter.Button(frame, text="Login", bg='#FF3399', fg="#FFFFFF", font=("Arial", 16))
 
 
 login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
