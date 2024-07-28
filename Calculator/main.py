ca9736472426e0b3f97bdb99da8b627469c5842c
Molly Brown <mollybrown616@yahoo.com>
2024-07-28 14:49:40 -0400

    Update main.py

--- Calculator/main.py
+++ Calculator/main.py
@@ -9,7 +9,8 @@ class Calculator:
 
      self.equation=StringVar()
      self.entry_value=''
-     Entry(width=17,bg='#fff',font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
+     self.solve=''
+     Entry(width=17,bg='#ccddff',font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
 
      Button(width=11,height=4,text='(',relief='flat',bg='white',command=lambda:self.show('(')).place(x=0 ,y=50)
      Button(width=11,height=4,text=')',relief='flat',bg='white',command=lambda:self.show(')')).place(x=90 ,y=50)
@@ -29,16 +30,16 @@ class Calculator:
      Button(width=11,height=4,text='-',relief='flat',bg='white',command=lambda:self.show('-')).place(x=270 ,y=200)
      Button(width=11,height=4,text='/',relief='flat',bg='white',command=lambda:self.show('/')).place(x=270 ,y=50)
      Button(width=11,height=4,text='x',relief='flat',bg='white',command=lambda:self.show('*')).place(x=270 ,y=125)
-     Button(width=11,height=4,text='=',relief='flat',bg='white',command=self.solve).place(x=270 ,y=350)
+     Button(width=11,height=4,text='=',relief='flat',bg='lightblue',command=self.solve).place(x=270 ,y=350)
      Button(width=11,height=4,text='C',relief='flat',command=self.clear).place(x=0 ,y=350)
      
      
      
-def show(self,value):
+ def show(self,value):
         self.entry_value+=str(value)
         self.equation.set(self.entry_value)
      
-def clear(self):
+ def clear(self):
         self.entry_value=''
         self.equation.set(self.entry_value)
 
