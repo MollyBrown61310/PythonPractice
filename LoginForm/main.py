6647c6745d7526f1067e4fc3b217132daf3dc6aa
Molly Brown <mollybrown616@yahoo.com>
2024-07-28 17:59:57 -0400

    Update main.py

--- LoginForm/main.py
+++ LoginForm/main.py
@@ -13,12 +13,12 @@ password_label = tkinter.Label(window, text="Password", bg='#333333', fg="#FFFFF
 login_button = tkinter.Button(window, text="Login", bg='#FF3399', fg="#FFFFFF", font=("Arial", 16))
 
 
-login_label.grid(row=0, column=0, columnspan=2, sticky="news")
+login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
 username_label.grid(row=1, column=0)
-username_entry.grid(row=1, column=1)
+username_entry.grid(row=1, column=1, pady=20)
 password_label.grid(row=2, column=0)
-password_entry.grid(row=2, column=1)
-login_button.grid(row=3, column=0, columnspan=2)
+password_entry.grid(row=2, column=1, pady=20)
+login_button.grid(row=3, column=0, columnspan=2, pady=30)
 
 
 
