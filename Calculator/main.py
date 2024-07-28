fc04c6196e470373b3ec1d0ec2a27e913fcc4869
Molly Brown <mollybrown616@yahoo.com>
2024-07-28 12:34:25 -0400

    Update main.py

---
+++ Calculator/main.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, Entry, Button , StringVar
+
+class Calculator:
+    def __init__(self,master): 
+     master.title("Calculator")
+     master.geometry('357x420+0+0')
+     master.config(bg='gray')
+     master.resizable(False,False)
+
+     self.equation+StringVar()
+     self.entry_value=''
+     Entry(width=17,bg='#fff',font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
+
+     def show(self,value):
+        self.entry_value+=str(value)
+        self.equation.set(self.entry_value)
+     
+     def clear(self):
+
+      root=Tk()
+      root.mainloop()
+ 
