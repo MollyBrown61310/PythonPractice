3f0fa4de7f14f2a56e3cca2956ea60a64eaa2268
Molly Brown <mollybrown616@yahoo.com>
2024-07-28 17:06:12 -0400

    Update main.py

--- LoginForm/main.py
+++ LoginForm/main.py
@@ -3,7 +3,14 @@ import tkinter
 window = tkinter.Tk()
 window.title("Login Form")
 window.geometry('340x440')
-window.configure(bg='#333333')
+#window.configure(bg='#333333')
+
+login_label = tkinter.Label(window, text="Login")
+
+
+
+
+login_label.grid(row=0, column=0)
 
 
 window.mainloop()
