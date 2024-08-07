b28ced93f59f1c689e5ac436aecdc1aee85828ea
Molly Brown <mollybrown616@yahoo.com>
2024-08-07 11:18:15 -0400

    Update main.py

---
+++ Chatbot/main.py
@@ -0,0 +1,6 @@
+from openai import OpenAI
+
+openai = OpenAI(
+    api_key='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',
+)
+
